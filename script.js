9cda14fb35f9117d9bfee24641a6074146e4b70c
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-11 00:33:53 -0300

    Adiciona a mudança da classe do bg clicando nos itens da lista (requisito 8)

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // capturar o texto do input e adicionar o texto do input ao item
 const getList = document.querySelector('#lista-tarefas'); // capturar a lista ordenada
 
-// adiciona a classe do background
-function addClassBg(event) {
-  event.target.classList.add('backgroundItens');
+// adiciona e muda a classe do background
+function changeClass(event) {
+  const getClassBg = document.querySelectorAll('.backgroundItens'); // capturar todos os elementos com a classe
+  for (let c = 0; c < getClassBg.length; c += 1) { // passar por todos os elementos
+    getClassBg[c].classList.remove('backgroundItens'); // remove a classe dos elementos que à contém
+  }
+  event.target.classList.add('backgroundItens'); // adiciona a classe ao elemento clicado
 }
 
 function addTask() {
@@ -12,7 +16,7 @@ function addTask() {
   getList.appendChild(createItem); // adiciona o item a lista
   createItem.innerHTML = getText.value; // item recebe o texto do input
   getText.value = ''; // apaga o conteudo do input
-  createItem.addEventListener('click', addClassBg); // adicionar um evento de clique, que muda o bg, no item criado
+  createItem.addEventListener('click', changeClass); // adicionar um evento de clique, que muda o bg, no item criado
 }
 
 // adicionar um evento click ao botão
